4ea026ba4f85a5b4d7326b132343363a4ab97352
MR-zn <846179626@qq.com>
2018-08-11 12:20:24 +0800

    fixed webpack bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-const path = require
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
-  entry: path.join(__dirname, 'src/main.js'),
+  entry: path.resolve(__dirname, 'src/main.js'),
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ],
+  mode: 'development'
 }
