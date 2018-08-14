ef3f9ea3ed36e32cd16df32d1cbcd1ea97a9c4c8
nan <846179626@qq.com>
2018-08-14 13:02:04 +0800

    completed Optimize directory structure

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,8 +1,14 @@
 const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const {
+  VueLoaderPlugin
+} = require('vue-loader')
+const webpack = require('webpack')
+const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
   target: 'web',
-  entry: path.resolve(__dirname, 'src/main.js'),
+  entry: path.resolve(__dirname, '../src/main.js'),
   output: {
     filename: 'bundle.[hash:8].js',
     path: path.resolve(__dirname, 'dist')
@@ -37,12 +43,17 @@ const config = {
         loader: 'url-loader',
         options: {
           limit: 1024,
-          name: '[name].[ext]'
+          name: 'resources/[path][name]-[md5:contenthash:hex:8].[ext]'
         }
       }]
     }
     ]
-  }
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HTMLPlugin()
+  ],
+  mode: process.env.NODE_ENV || 'production'
 }
 
 module.exports = config
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,80 +1,100 @@
+const webpack = require('webpack')
 const path = require('path')
+const ExtractTextWebapckPlugin = require('extract-text-webpack-plugin')
+
+const HTMLPlugin = require('html-webpack-plugin')
 const {
   VueLoaderPlugin
 } = require('vue-loader')
-const HTMLPlugin = require('html-webpack-plugin')
-const ExtractTextWebapckPlugin = require('extract-text-webpack-plugin')
+
 const merge = require('webpack-merge')
-
-const webpack = require('webpack')
-
+const baseConfig = require('./webpack.config.base');
 const isDev = process.env.NODE_ENV === 'development'
 
-let config
+const defaultPlugins = [
+  // make sure to include the plugin for the magic
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
+  new VueLoaderPlugin(),
+  new HTMLPlugin()
+]
 
+let config
+const devServer = {
+  // 很重要
+  useLocalIp: true,
+  host: '0.0.0.0',
+  port: '9797',
+  overlay: {
+    errors: true
+  },
+  // 自动打开浏览器
+  open: true,
+  // 热更新
+  hot: true
+  // historyFallback: {}
+}
 if (isDev) {
-  config.module.rules.push({
-    test: /\.css$/,
-    use: [
-      'vue-style-loader',
-      'css-loader'
+  config = merge(baseConfig, {
+    // devtool = '#cheap-module-eval-source-map',
+    module: {
+      rules: [{
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }]
+    },
+    devServer,
+    plugins: [new webpack.HotModuleReplacementPlugin(),
+      // new webpack.NoEmitOnErrorsPlugin()
     ]
   })
-  config.devtool = '#cheap-module-eval-source-map'
-  config.devServer = {
-    // 很重要
-    useLocalIp: true,
-    host: '0.0.0.0',
-    port: '9797',
-    overlay: {
-      errors: true
-    },
-    // 自动打开浏览器
-    open: true,
-    // 热更新
-    hot: true
-    // historyFallback: {}
-  }
-  config.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  )
 } else {
-  config.entry = {
-    app: path.join(__dirname, 'src/main.js'),
-    vendor: ['vue']
-  }
-  config.output.filename = '[name].[chunkhash:8].js'
-  config.module.rules.push({
-    test: /\.css$/,
-    use: ExtractTextWebapckPlugin.extract({
-      use: 'css-loader'
-    })
-
-  })
-  config.plugins.push(
-    new ExtractTextWebapckPlugin('styles.[md5:contenthash:hex:20].css')),
-  config.optimization = {
-
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          chunks: 'initial',
-          minChunks: 2,
-          maxInitialRequests: 5,
-          minSize: 0
-        },
-        vendor: {
-          test: /node_modules/,
-          chunks: 'initial',
-          name: 'vendor',
-          name: 'runtime',
-          priority: 10,
-          enforce: true
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, '../src/main.js'),
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:8].js'
+    },
+    module: {
+      rules: [{
+        test: /\.css$/,
+        use: ExtractTextWebapckPlugin.extract({
+          use: 'css-loader'
+        })
+      }]
+    },
+    plugins: defaultPlugins.concat([
+      new ExtractTextWebapckPlugin('styles.[md5:contenthash:hex:20].css')
+    ]),
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            chunks: 'initial',
+            minChunks: 2,
+            maxInitialRequests: 5,
+            minSize: 0
+          },
+          vendor: {
+            test: /node_modules/,
+            chunks: 'initial',
+            name: 'vendor',
+            name: 'runtime',
+            priority: 10,
+            enforce: true
+          }
         }
       }
     }
-  }
+  })
 }
 
 module.exports = config
